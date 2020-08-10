f234e226d8da69c2c1617bb2d48be532047e2c43
wqj <991575842@qq.com>
2020-08-10 23:54:02 +0800

    master 2020-08-10 23:53 springCloud 初始化

---
+++ cloud-provider-payment8001/src/main/java/com/atom/springcloud/controller/PaymentController.java
@@ -0,0 +1,42 @@
+package com.atom.springcloud.controller;
+
+import com.atom.springcloud.entities.CommonResult;
+import com.atom.springcloud.entities.Payment;
+import com.atom.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(Payment payment){
+        int result = paymentService.create(payment);
+        log.info("*****插入结果："+result);
+        if (result>0){
+            return new CommonResult(200,"插入成功",result);
+        }else {
+            return new CommonResult(4001,"插入失败",null);
+        }
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentByid(@PathVariable("id") Long id){
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("*****查询结果："+payment);
+        if (payment!=null){
+            return new CommonResult(200,"查询成功",payment);
+        }else {
+            return new CommonResult(4001,"没有对应记录,查询ID"+id,null);
+        }
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/atom/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,24 @@
+package com.atom.springcloud.service.impl;
+
+import com.atom.springcloud.dao.PaymentDao;
+import com.atom.springcloud.entities.Payment;
+import com.atom.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Resource
+    private PaymentDao paymentDao;
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
